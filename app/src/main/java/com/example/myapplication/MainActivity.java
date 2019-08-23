895cf3eb0ff4a5c322243dd047a7d05b5ee9a1b9
zulker01 <zulker02@gmail.com>
2019-08-23 15:33:35 +0600

    on 23/08/19 15:33 for first page

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static java.lang.System.exit;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+        Button helpButton = (Button) findViewById(R.id.helpButton);
+        Button quitButton = (Button) findViewById(R.id.quitButton);
+
+        helpButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Toast helpToast = Toast.makeText(MainActivity.this,R.string.helpToast, Toast.LENGTH_LONG);
+                helpToast.show();
+            }
+        }
+        );
+
+        quitButton.setOnClickListener(new View.OnClickListener()
+                                      {
+                                          @Override
+                                          public void onClick(View view)
+                                          {
+                                              Toast quitToast = Toast.makeText(MainActivity.this,R.string.quitToast, Toast.LENGTH_LONG);
+                                              quitToast.show();
+                                              exit(0);
+                                          }
+                                      }
+        );
+    }
+}
